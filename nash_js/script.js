33134b712a0e6d48c6f925e3f4021d07792aded0
vladimir.avetian@sciencespo.fr <vladimir.avetian@sciencespo.fr>
2024-01-19 18:51:39 +0100

    leaflet integrated

--- nash_js/script.js
+++ nash_js/script.js
@@ -1,74 +1,76 @@
 $(document).ready(function() {
+    // Initialize the Leaflet map
+    var map = L.map('map', {
+    zoomControl: false, // Disable the zoom control buttons
+    scrollWheelZoom: false, // Disable zooming with the mouse scroll wheel
+    doubleClickZoom: false, // Disable zooming with double-click
+    touchZoom: false, // Disable zooming with touch gestures
+    boxZoom: false, // Disable zooming with box selection
+    keyboard: false, // Disable zooming with keyboard (+ and - keys)
+    dragging: false // Disable dragging to pan
+    }).setView([48.858, 2.345], 12); // Set the initial view
 
-    $('.change-image').click(function() {
-        var newImage = $(this).data('image');
-        $('#interactive-image').attr('src', newImage);
+    // var map = L.map('map').setView([48.858, 2.345], 12); // Coordinates for Paris
 
-        var arrText = "";
-        switch(newImage) {
-            case 'img/paris_buildings_arrond_1.png':
-                arrText = "Names of places: 1 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_2.png':
-                arrText = "Names of places: 2 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_3.png':
-                arrText = "Names of places: 3 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_4.png':
-                arrText = "Names of places: 4 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_5.png':
-                arrText = "Names of places: 5 Arrondissement";
-                break;               
-            case 'img/paris_buildings_arrond_6.png':
-                arrText = "Names of places: 6 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_7.png':
-                arrText = "Names of places: 7 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_8.png':
-                arrText = "Names of places: 8 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_9.png':
-                arrText = "Names of places: 9 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_10.png':
-                arrText = "Names of places: 10 Arrondissement";
-                break;
-            case 'img/paris_buildings_arrond_11.png':
-                arrText = "Names of places: 11 Arrondissement";
-                break;                                                                                               
-            case 'img/paris_buildings_arrond_12.png':
-                arrText = "Names of places: 12 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_13.png':
-                arrText = "Names of places: 13 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_14.png':
-                arrText = "Names of places: 14 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_15.png':
-                arrText = "Names of places: 15 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_16.png':
-                arrText = "Names of places: 16 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_17.png':
-                arrText = "Names of places: 17 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_18.png':
-                arrText = "Names of places: 18 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_19.png':
-                arrText = "Names of places: 19 Arrondissement";
-                break;                                 
-            case 'img/paris_buildings_arrond_20.png':
-                arrText = "Names of places: 20 Arrondissement";
-                break;                                                                                                                                                                                                                                 
-            default:
-                arrText = "Names of places: Paris";
-        }
-        $('#overlay-text').text(arrText);
-    });
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '© OpenStreetMap contributors, © CARTO'
+    }).addTo(map);
+
+
+    var imageUrl = 'img/paris2leaf.png'; // URL to your georeferenced image
+    var imageBounds = [[48.816277, 2.225594], [48.902098, 2.468296]]; // Bounds of the image in [lat, lng] format
+
+    L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+    // Function to set the initial style of each polygon
+    function style(feature) {
+        return {
+            color: 'transparent',  // Transparent border
+            weight: 0,             // No border
+            fillColor: 'transparent', // Transparent fill
+            fillOpacity: 0         // Fully transparent fill
+        };
+    }
+
+    // Function to highlight feature on mouseover and click
+    function highlightFeature(e) {
+        var layer = e.target;
+        layer.setStyle({
+            weight: 2,
+            color: 'blue',     // Change border color to blue
+            dashArray: '',
+            fillOpacity: 0
+        });
+        var arrondissement = layer.feature.properties.c_ar; 
+        $('#overlay-text').text("Names of places: " + arrondissement + " Arrondissement");
+    }
+
+    // Function to reset the highlight
+    function resetHighlight(e) {
+        geojsonLayer.resetStyle(e.target);
+    }
+
+    // Function to handle click on feature
+    function onFeatureClick(e) {
+        var layer = e.target;
+        var arrondissement = layer.feature.properties.c_ar; 
+        $('#overlay-text').text("Names of places: " + arrondissement + " Arrondissement");
+    }
+
+    // Function to handle each feature
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: onFeatureClick
+        });
+    }
+
+    // Load the GeoJSON and add it to the map
+    var geojsonLayer = L.geoJSON.ajax('data/arrondissements.geojson', {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
 });
+
+
